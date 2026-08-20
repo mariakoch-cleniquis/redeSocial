ab46fe310d58f102ac274441a10033a1813f8601
mariakoch-cleniquis <maria.oliveira.koch@escola.pr.gov.br>
2026-08-20 08:48:51 -0300

    ok

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
-//Botão curtidas
-document.addEventListener
+//Botao curtidas
+document.addEventListener("DOMContentLoaded", () =>{
+const likeBtn = document.querySelector(".left-actions .action-btn:first-child");
+if (!likeBtn) return;
+const likeSvg = likeBtn.querySelector("svg");
+
+
+//localiza o contador
+
+let textNode = Array.from(likeBtn.childNodes).find(node) => node.nodeType
+=== Node.TEXT_NODE && node.textContent.trim()!== ""
+);
+
+//zera o contador
+let cont = 0;
+
+//atualiza
+if(textNode){
+    textNode.textContent = `0`;
+}
+
+//coração
+function applyLikedStyle(){
+likeSvg.style.fill ="#ef4444";
+likeSvg.style.stroke ="#ef4444";
+likeSvg.style.color ="#ef4444";
+
+   
+//efeito curtida
+likeSvg.style.transform ="scale(1.3)";
+setTimeout(() => (likeSvg.style.transform="scale(1)")150)
+}
+
+
+
+
+
+
+
+
+
+
+})
